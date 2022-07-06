0dce1a9f7c3d5e4c09a09a06d8909b9e73c97286
Pietro Piva Vieira <pietro.developer@gmail.com>
2022-07-06 18:12:25 -0300

    First Commit

---
+++ src/scripts.js
@@ -0,0 +1,14 @@
+VANTA.NET({
+  el: "#net-background",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xff2b,
+  backgroundAlpha: 0.0,
+  points: 10.00,
+  showDots: false,
+});
